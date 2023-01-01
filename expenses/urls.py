e96139712aee51ffc1a68e7a3659a0ea05ffa2ed
GabrielLin2022 <sam90044@yahoo.com.tw>
2023-01-01 17:51:19 +0800

    add

--- expenses/urls.py
+++ expenses/urls.py
@@ -6,3 +6,4 @@ urlpatterns = [
     path('update/<str:pk>', views.update, name='Update'),
     path('delete/<str:pk>', views.delete, name='Delete')
 ]
+#<str:pk>:可以吃字串的網址
--- expenses/views.py
+++ expenses/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render, redirect
 from .models import Expense
 from .forms import ExpenseModelForm
 
-
+#=================================================新增
 def index(request):
 
     expenses = Expense.objects.all()  # 查詢所有資料
 
-    form = ExpenseModelForm()
+    form = ExpenseModelForm() #建立實體物件
 
     if request.method == "POST":
         form = ExpenseModelForm(request.POST)
@@ -23,15 +23,15 @@ def index(request):
 
     return render(request, 'expenses/index.html', context)
 
-
+#=================================================修改
 def update(request, pk):
 
-    expense = Expense.objects.get(id=pk)
+    expense = Expense.objects.get(id=pk)#從資料表取得id, 指給pk, 再傳上去 update(pk)
 
-    form = ExpenseModelForm(instance=expense)
+    form = ExpenseModelForm(instance=expense)##instance 表拿取 expense 內部資料，也就是從資料庫經由objects取得的資料
 
     if request.method == 'POST':
-        form = ExpenseModelForm(request.POST, instance=expense)
+        form = ExpenseModelForm(request.POST, instance=expense) #instance 表拿取 expense 內部資料，也就是從資料庫經由objects取得的資料
         if form.is_valid():#Form的對象主要任務是驗證data,使用is_valid()來進行驗證，它會回傳boolean
             form.save()
         return redirect('/expenses')
@@ -42,7 +42,7 @@ def update(request, pk):
 
     return render(request, 'expenses/update.html', context)
 
-
+#=================================================刪除
 def delete(request, pk):
 
     expense = Expense.objects.get(id=pk)
